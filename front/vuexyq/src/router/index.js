181044efc78b6da1d438b44c682a1441cc6e5418
tjj <2512080806@qq.com>
2020-12-21 19:10:06 +0800

    推

--- front/vuexyq/src/router/index.js
+++ front/vuexyq/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import selectresdence from '@/components/Tjj/Selectresdence'
+import updateresdence from '@/components/Tjj/Updateresdence'
+import insertresdence from '@/components/Tjj/Insertresdence'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/sres',
+      name: 'selectresdence',
+      component: selectresdence
+    },
+    {
+      path: '/ures',
+      name: 'updateresdence',
+      component: updateresdence
+    },
+    {
+      path: '/ires',
+      name: 'insertresdence',
+      component: insertresdence
     }
   ]
 })
